559266cb241e8b6f6d50ab55c8128899b268e176
Phillip <renaudtp@oregonstate.edu>
2025-12-09 19:22:36 -0600

    Improved placement of robot in map

--- main.py
+++ main.py
@@ -4,13 +4,18 @@ from planner import HybridAStar
 from viz import visualize_and_mcap
 import time
 
-def load_grid():
-    grid = np.zeros((50, 50), dtype=np.uint8)
-
+def load_grid(rows=50, cols=50):
+    # grid = np.zeros((rows, cols), dtype=np.uint8)
     # Add obstacles
-    grid[25, 35] = 1
-    grid[35, 12] = 1
-    grid[10, 20] = 1
+    grid = np.random.choice([0, 1], size=(rows, cols), p=[0.75, 0.25])
+    # Find a free cell to place the robot
+    free_indices = np.argwhere(grid == 0)
+    if free_indices.size > 0:
+        selected = free_indices[np.random.randint(len(free_indices))]
+        i, j = selected
+        # Center the robot in the cell
+        x_coord = j + 0.5
+        y_coord = i + 0.5
 
     # Boundary walls
     grid[:1, :] = 1   # top wall
@@ -18,19 +23,19 @@ def load_grid():
     grid[:, :1] = 1   # left wall
     grid[:, -1:] = 1  # right wall
 
-    return grid
+    return grid, x_coord, y_coord
 
 def main():
-    grid = load_grid()
+    grid, x_coord, y_coord = load_grid()
 
     # Test explored nodes
     explored = [RobotState(i, j, 0, 0) for i in range(0, 20, 2) for j in range(0, 20, 2)]
 
     # Test path
-    path = [RobotState(i, i, 45, 0.1) for i in range(5, 25)]
+    path = [RobotState(x_coord+ i, y_coord + i, 45, 0.1) for i in range(5, 25)]
 
     # Robot model
-    robot_model = RobotModel()
+    robot_model = RobotModel(length=0.85, width=0.45)
 
     visualize_and_mcap(grid, path, explored, robot_model, "hybrid_astar_single_tick_main.mcap")
 
